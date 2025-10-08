072393cc6ab2e4d5889b574b1ff450a21efe878f
Geril Valdo <gerilmanday@gmail.com>
2025-10-08 09:27:18 +0700

    Done Tugas 3

---
+++ bin/tugas_3.dart
@@ -0,0 +1,50 @@
+import 'dart:io';
+
+void main() {
+  List<String> buah = ['Apel', 'Jeruk', 'Mangga', 'Anggur'];
+  List<String> daftarBelanja = ['Beras', 'Minyak', 'Gula', 'Telur'];
+
+  void cetakBintang(int n) {
+    for (n; n <= 5; n++) {
+      stdout.write('*');
+    }
+  }
+
+  void cetakGanjil(int n) {
+    for (n; n <= 20; n += 2) {
+      stdout.write('$n ');
+    }
+  }
+
+  void namaBerulang(String nama, int n) {
+    while (n <= 5) {
+      print(nama);
+      n++;
+    }
+  }
+
+  void cetakBuah(List<String> buah) {
+    for (var cetak in buah) {
+      print('Saya suka $cetak');
+    }
+  }
+
+  void cetakBelanja(List<String> daftarBelanja, int index) {
+    while (index < daftarBelanja.length) {
+      for (var belanja in daftarBelanja) {
+        print('Item ke-$index: $belanja');
+        index++;
+      }
+    }
+  }
+
+  cetakGanjil(1);
+  print('\n');
+  cetakBintang(1);
+  print('\n');
+  namaBerulang("Geril Valdo Jatsiah Manday", 1);
+  print('\n');
+  cetakBuah(buah);
+  print('\n');
+  cetakBelanja(daftarBelanja, 1);
+}
